8f5195b75b3cab79196c316faeafd6fe6f690e43
devillkaow <devillkaow@gmail.com>
2021-02-18 11:08:27 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
-import React,{ useState } from 'react'
+import React from 'react'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom'
-import { Reset } from 'styled-reset'
 import styled from 'styled-components'
 import { PostsLane } from './component/lane/Post'
 import { NavbarLane } from './component/lane/Navbar'
 import { UserInfo } from './component/lane/UserInfo'
 import { ViewPost } from "./features/post/ViewPost";
+import GlobalStyle  from './component/GlobalStyle'
 
 import Login from './component/login/Login'
 
 import useToken from './useToken';
 
-const Content=styled.div`
+const Content = styled.div`
 display: flex;
+height: auto;
+  background-color: rgba(	51, 162, 255, .2);
 `
 
 function App() {
   const { token, setToken } = useToken();
 
-  if(!token) {
+  if (!token) {
     return <Login setToken={setToken} />
-  
+
   }
 
   return (
     <>
-    <Reset />
-    <Router>
-      <NavbarLane/>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={() => (
-              <React.Fragment>
-                <Content>
-                  <PostsLane/>
-                  <UserInfo/>
-                </Content>
-              </React.Fragment>
-            )}
-          />
-          <Route exact path="/posts/:postId" component={ViewPost} />
-          <Redirect to="/" />
-        </Switch>
-      
-    </Router>
+      <GlobalStyle />
+      <Router>
+        <NavbarLane />
+        <Content>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <React.Fragment>
+                  <PostsLane />
+                  <UserInfo />
+                </React.Fragment>
+              )}
+            />
+            <Route exact path="/posts/:postId" component={ViewPost} />
+            <Redirect to="/" />
+          </Switch>
+        </Content>
+      </Router>
     </>
   )
 }
