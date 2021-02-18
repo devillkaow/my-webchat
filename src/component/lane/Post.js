f898279be8e68ec04ffeafcb08d83c749e3c2ac8
devillkaow <devillkaow@gmail.com>
2021-02-18 11:09:12 +0700

    update Post.js

--- src/component/lane/Post.js
+++ src/component/lane/Post.js
@@ -3,16 +3,18 @@ import { AddPostForm } from '../../features/post/AddPostForm'
 import { PostsList } from '../../features/post/PostList'
 
 const PostsContainer = styled.div`
-width:66%;
-padding:2rem;
+  width:66%;
+  height: auto;
+  padding:3rem;
+  background-color: rgba(	51, 162, 255, .1);
 `
 
 export const PostsLane = () => {
   return (
     <>
       <PostsContainer>
-      <AddPostForm />
-      <PostsList />
+        <AddPostForm />
+        <PostsList />
       </PostsContainer>
     </>
   );
