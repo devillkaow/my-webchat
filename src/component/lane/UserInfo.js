5b029612041781aa4c50162e21c96feb1ef4f66c
devillkaow <devillkaow@gmail.com>
2021-02-18 11:09:57 +0700

    update User

--- src/component/lane/UserInfo.js
+++ src/component/lane/UserInfo.js
@@ -1,19 +1,30 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
-import Login from '../login/Login';
 
 const UserInfoContainer = styled.div`
-width:33%;
-padding:2rem;
+  width:33%;
+  height: auto;
+  padding:3rem;
+  background-color: rgba(	51, 162, 255, .7);
 `
 
 export const UserInfo = () => {
-  // const [loggedUsername, setLoggedUsername] = useState(Login);
-  // console.log(loggedUsername)
+
+  const userlogged = useSelector((state) => state.userlogged)
+  // const users = useSelector((state) => state.users)
+
+  const users = useSelector((state) =>
+    state.users.find((user) => user.username == userlogged[0].username.username )
+  )
+  console.log('userlogged',userlogged)
+  console.log('users',users)
   return (
     <>
       <UserInfoContainer>
-        {/* {loggedUsername} */}
+       <p>{users.name}</p>
+       <p>{users.age}</p>
+       <p>Blank~~!</p>
       </UserInfoContainer>
     </>
   );
--- src/features/user/usersSlice.js
+++ src/features/user/usersSlice.js
@@ -1,9 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = [
-  { id: '0', name: 'Stewie suie', username: 'stewie', password: 'suie' },
-  { id: '1', name: 'kaow kaow', username: 'kaow', password: 'kaow' },
-  { id: '2', name: '000000000', username: 'zero', password: 'zero' },
+  { 
+    id: '0',
+    name: 'Stewie suie', 
+    username: 'stewie', 
+    password: 'suie',
+    age: '19',
+    principles: '' 
+  },
+  { 
+    id: '1', 
+    name: 'kaow kaow', 
+    username: 'kaow', 
+    password: 'kaow',
+    age: '20',
+    principles: '' 
+  },
+  { id: '2', 
+    name: '000000000', 
+    username: 'zero', 
+    password: 'zero',
+    age: '21',
+    principles: '' 
+  },
 ]
 
 const usersSlice = createSlice({
