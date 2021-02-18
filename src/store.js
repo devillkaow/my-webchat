ec68d94cb6b53215fe61e5d5155957a2d992a01c
devillkaow <devillkaow@gmail.com>
2021-02-18 11:08:36 +0700

    update store.js

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from './features/post/postsSlice'
 import usersReducer from './features/user/usersSlice'
+import userReducer from './features/user/userLogged'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: usersReducer,
+    userlogged: userReducer
   },
 })
