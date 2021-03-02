778c31f3a8ec0be885c5fbea8a8082060dbd9fd4
devillkaow <devillkaow@gmail.com>
2021-03-02 11:39:00 +0700

    add UserInformationLane

--- src/component/lane/UserInfo.js
+++ src/component/lane/UserInfo.js
@@ -1,30 +1,37 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux'
 import styled from 'styled-components'
+
 
 const UserInfoContainer = styled.div`
   width:33%;
   height: auto;
   padding:3rem;
-  background-color: rgba(	51, 162, 255, .7);
 `
+const Text = styled.div`
+font-size: 30px;
+display: flex;
+justify-content: left;
+margin: 18px 0;
+`
+
 
 export const UserInfo = () => {
 
   const userlogged = useSelector((state) => state.userlogged)
-  // const users = useSelector((state) => state.users)
 
   const users = useSelector((state) =>
-    state.users.find((user) => user.username == userlogged[0].username.username )
+    state.users.find((user) => user.username === userlogged[0].username )
   )
+
   console.log('userlogged',userlogged)
   console.log('users',users)
   return (
     <>
       <UserInfoContainer>
-       <p>{users.name}</p>
-       <p>{users.age}</p>
-       <p>Blank~~!</p>
+       <Text>Name : {users.name}</Text>
+       <Text>Age : {users.age}</Text>
+       <Text>Blank~~!</Text>
       </UserInfoContainer>
     </>
   );
