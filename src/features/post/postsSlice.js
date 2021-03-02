c2a73e826c376499689f4520a246b8fc04148aa9
devillkaow <devillkaow@gmail.com>
2021-03-02 11:40:34 +0700

    add postreducer

--- src/features/post/postsSlice.js
+++ src/features/post/postsSlice.js
@@ -8,7 +8,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload)
+        state.unshift(action.payload)
       },
       prepare(content, userId) {
         return {
@@ -17,33 +17,65 @@ const postsSlice = createSlice({
             content,
             user: userId,
             reactions: {
+              userThumbsUp: [],
               thumbsUp: 0,
-              heart: 0,
+              // userHeart: [],
+              // heart: 0,
             },
-            comment: []
+            comments: []
           },
         }
       },
     },
     reactionAdded(state, action) {
-      const { postId, reaction } = action.payload
+      const { postId, reaction, user } = action.payload
       const existingPost = state.find((post) => post.id === postId)
-      if ( existingPost.reactions[reaction] <= 0 ) {
+      const findUser = existingPost.reactions.userThumbsUp.filter(users => users == user)
+      const userr =  (element) => element == user;
+      if(findUser.length == 0){
+        existingPost.reactions.userThumbsUp.push(user)
         existingPost.reactions[reaction]++
-      } 
-      //เอาออกตอนทำuser
+      }
       else {
+        const alreadyLiked = existingPost.reactions.userThumbsUp.findIndex(userr)
+        existingPost.reactions.userThumbsUp.splice(alreadyLiked, 1)
         existingPost.reactions[reaction]--
       }
+
+      console.log('find',findUser)
+      console.log('find',findUser.length)
+
+
+      // if ( existingPost.reactions[reaction] <= 0 ) {
+      //   existingPost.reactions[reaction]++
+      // } 
+      /* //////////เอาออกตอนทำuser////////// */
+      // else {
+      //   existingPost.reactions[reaction]--
+      // }
     },
     commentAdded(state, action) {
-      const { postId, comments } = action.payload
+      const { postId, comment ,userComment} = action.payload
       const existingPost = state.find((post) => post.id === postId)
-      existingPost.comment.push(comments)
+      existingPost.comments.push({commentcontents:comment,userComments:userComment})
+      // existingPost.comments.push({userComments:userComment})
+
     },
+    postDeleted(state, action){
+      const { postId } = action.payload
+
+      console.log("postIdรับ",postId)
+      
+      const postIndex = state.findIndex((post) => post.id == postId)
+
+      console.log("postIndex",postIndex)
+
+      state.splice(postIndex, 1)
+    }
+
 },
 })
-export const { postAdded, postUpdated, reactionAdded, commentAdded, postDeleted } = postsSlice.actions
+export const { postAdded, reactionAdded, commentAdded, postDeleted } = postsSlice.actions
 
 
 export default postsSlice.reducer
